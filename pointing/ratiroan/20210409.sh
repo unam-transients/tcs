d5f83fd853bc6844e8e9d9bf0ddfd1990f4cbb91
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2021-04-13 08:55:28 -0500

    Selected correct relative models.
    The relative models that we want are finder against C1 not C1 against finder.

--- pointing/ratiroan/20210409.sh
+++ pointing/ratiroan/20210409.sh
@@ -1,7 +1,7 @@
 make clean
 ABSOLUTE=0 make
-sh relative.sh ratiroan/20210409C1.txt ratiroan/20210409ne.txt | ./fitmodel | tee ratiroan/20210409C1ne.log
-sh relative.sh ratiroan/20210409C1.txt ratiroan/20210409se.txt | ./fitmodel | tee ratiroan/20210409C1se.log
+sh relative.sh ratiroan/20210409ne.txt ratiroan/20210409C1.txt | ./fitmodel | tee ratiroan/20210409neC1.log
+sh relative.sh ratiroan/20210409se.txt ratiroan/20210409C1.txt | ./fitmodel | tee ratiroan/20210409seC1.log
 make clean
 ABSOLUTE=1 make
 sh select.sh 0 ratiroan/20210409C1.txt | ./fitmodel | tee ratiroan/20210409C1.log
