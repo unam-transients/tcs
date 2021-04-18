c7ace1967e07658b53b2bb9be4de1cd7ecbae2b3
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2021-04-18 14:20:48 -0500

    Blocks for Carlos Román for 2021A.

---
+++ tcs/blocks-ddotioan/roman.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+sed '/^#/d' <<EOF |
+# 10 05:32:28.64 -02:34:42.0 -4h -2h OSW
+# 11 05:32:28.64 -02:34:42.0 -1h +1h OSW
+# 12 05:32:28.64 -02:34:42.0 +2h +4h OSW
+# 20 06:00:00.78 -02:30:38.2 -4h -2h OSE
+# 21 06:00:00.78 -02:30:38.2 -1h +1h OSE
+# 22 06:00:00.78 -02:30:38.2 +2h +4h OSE
+# 30 05:32:44.69 +08:11:51.2 -4h -2h ONW
+# 31 05:32:44.69 +08:11:51.2 -1h +1h ONW
+# 32 05:32:44.69 +08:11:51.2 +2h +4h ONW
+# 40 05:59:34.30 +08:11:37.8 -4h -2h ONE
+# 41 05:59:34.30 +08:11:37.8 -1h +1h ONE
+# 42 05:59:34.30 +08:11:37.8 +2h +4h ONE
+50 11:15:46.70 +87:39:50.9 -4h +4h Polaris
+60 11:13:59.30 +75:46:17.5 -4h +4h Spider
+70 09:33:52.60 +68:37:39.0 -4h +4h Ursa01
+80 08:33:22.50 +61:00:11.2 -4h +4h Ursa02
+EOF
+while read blockid alpha delta minha maxha name
+do
+cat >2001-roman-$blockid <<EOF
+{
+  "project": {
+    "identifier": "2001",
+    "name": "Roman"
+  },
+  "identifier": "$blockid",
+  "name": "$name",
+  "visits": [
+    {
+      "identifier": "2000",
+      "name": "focusing",
+      "targetcoordinates": {
+        "type"   : "equatorial",
+        "alpha"  : "$alpha",
+        "delta"  : "$delta",
+        "equinox": "2000"
+      },
+      "command": "focusvisit",
+      "estimatedduration": "5m"
+    },
+    {
+      "identifier": "0",
+      "name": "science exposures",
+      "targetcoordinates": {
+        "type"   : "equatorial",
+        "alpha"  : "$alpha",
+        "delta"  : "$delta",
+        "equinox": "2000"
+      },
+      "command": "gridvisits 1 5 1 60",
+      "estimatedduration": "6m"
+    }
+  ],
+  "constraints": {
+    "maxskybrightness": "nauticaltwilight",
+    "minha": "$minha",
+    "maxha": "$maxha",
+    "minmoondistance": "30d",
+    "maxfocusdelay": "3600"
+  },
+  "persistent": "false"
+}
+
+EOF
+done
