740c122008dbeb3edc4063c27c05ff28a5479ecc
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2021-05-17 10:14:57 -0500

    Update direct-ddoti.sh

--- direct/direct-ddoti.sh
+++ direct/direct-ddoti.sh
@@ -12,7 +12,7 @@ cat <<"EOF"
 <link rel="stylesheet" href="../style.css" type="text/css"/>
 <meta name=viewport content="width=device-width, initial-scale=1">
 <title>
-ddoti: Pipeline Analysis
+DDOTI: Pipeline Analysis
 </title>
 </head>
 <body>
