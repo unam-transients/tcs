c8f690ee7610fc0ea701cc7d99654fdcb45dbd2c
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2022-02-23 18:11:55 -0600

    Set $PATH in crontab.

--- tcs/sys/install-project-coatlioan.sh
+++ tcs/sys/install-project-coatlioan.sh
@@ -65,14 +65,15 @@ sudo mv /etc/hosts.tmp /etc/hosts
 # crontab
 
 (
+  echo 'PATH=/usr/local/bin:/usr/bin:/bin'
   echo 'MAILTO=""'
 
   cat <<"EOF"
-00 21 *  *  *  /usr/local/bin/tcs cleanfiles
-*  *  *  *  *  /usr/local/bin/tcs updatevarlatestlink
-*  *  *  *  *  /usr/local/bin/tcs updatelocalsensorsfiles
-*  *  *  *  *  /usr/local/bin/tcs checkreboot
-*  *  *  *  *  /usr/local/bin/tcs checkrestart
+00 21 *  *  *  tcs cleanfiles
+*  *  *  *  *  tcs updatevarlatestlink
+*  *  *  *  *  tcs updatelocalsensorsfiles
+*  *  *  *  *  tcs checkreboot
+*  *  *  *  *  tcs checkrestart
 EOF
 
   case $host in
@@ -81,28 +82,28 @@ EOF
     ;;  
   *)
     cat <<"EOF"
-*  *  *  *  *  /usr/local/bin/tcs checkhalt
+*  *  *  *  *  tcs checkhalt
 EOF
   esac
 
   case $host in
   control)
     cat <<"EOF"
-00 00 *  *  *  /usr/local/bin/tcs updatevarlatestlink
-*  *  *  *  *  sleep 10; /usr/local/bin/tcs updatesensorsfiles services control platform instrument
-*  *  *  *  *  /usr/local/bin/tcs updateweatherfiles-oan
-00 18 *  *  *  /usr/local/bin/tcs updateweatherfiles-oan -a
+00 00 *  *  *  tcs updatevarlatestlink
+*  *  *  *  *  sleep 10; tcs updatesensorsfiles services control platform instrument
+*  *  *  *  *  tcs updateweatherfiles-oan
+00 18 *  *  *  tcs updateweatherfiles-oan -a
 *  *  *  *  *  mkdir -p /usr/local/var/tcs/alerts /usr/local/var/tcs/oldalerts; rsync -aH /usr/local/var/tcs/alerts/. /usr/local/var/tcs/oldalerts/.
 00 *  *  *  *  rsync -aH /usr/local/var/tcs/ rsync://transients.astrossp.unam.mx/coatli-raw/
 *  *  *  *  *  rsync -aH --delete /usr/local/var/tcs/selector rsync://transients.astrossp.unam.mx/coatli-raw/
-00 00 *  *  *  /usr/local/bin/tcs fetchblocks
-01 00 *  *  *  /usr/local/bin/tcs loadblocks
+00 00 *  *  *  tcs fetchblocks
+01 00 *  *  *  tcs loadblocks
 EOF
     ;;
   services)
     cat <<"EOF"
 */5 *  *  *  *  sh /usr/local/var/www/tcs/plots.sh
-*/5 *  *  *  * /usr/local/bin/tcs logsensors
+*/5 *  *  *  * tcs logsensors
 *   *  *  *  *  rsync -aH --include="error.txt" --include="warning.txt" --include="summary.txt" --include="info.txt" --include="*/" --exclude="*" /usr/local/var/tcs/ rsync://transients.astrossp.unam.mx/coatli-raw/
 00  *  *  *  *  rsync -aH /usr/local/var/tcs/ rsync://transients.astrossp.unam.mx/coatli-raw/
 */5 *  *  *  *  rsync -aH --remove-source-files --include="*/" --include="*.fits.*" --exclude="*" /usr/local/var/tcs/ rsync://transients.astrossp.unam.mx/coatli-raw/
