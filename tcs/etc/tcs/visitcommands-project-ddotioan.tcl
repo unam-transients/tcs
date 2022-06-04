ddc36484f85a9fdd75b85077ba592f8a5763fc04
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2022-06-04 01:05:31 -0700

    Tweak focus parameters.

--- tcs/etc/tcs/visitcommands-project-ddotioan.tcl
+++ tcs/etc/tcs/visitcommands-project-ddotioan.tcl
@@ -569,15 +569,15 @@ proc initialfocusvisit {} {
   executor::setbinning 4
   log::summary "initialfocusvisit: focusing with binning 4."
   executor::waituntiltracking
-  executor::focus 4000 400 false 1
+  executor::focus 4000 500 true 1
   executor::setwindow "2kx2k"
   executor::setbinning 2
   log::summary "initialfocusvisit: focusing with binning 2."
-  executor::focus 1000 100 false 2
+  executor::focus 2000 250 true 2
   executor::setwindow "1kx1k"
   executor::setbinning 1
   log::summary "initialfocusvisit: focusing with binning 1."
-  executor::focus 1000 100 true 4
+  executor::focus 2000 250 true 4
   executor::setfocused
 
   log::summary "initialfocusvisit: taking tilt witness."
@@ -615,16 +615,15 @@ proc focusvisit {} {
   executor::setbinning 4
   executor::waituntiltracking
   log::summary "focusvisit: focusing with binning 4."
-  executor::focus 4000 400 false 1
+  executor::focus 4000 500 true 1
   executor::setwindow "2kx2k"
   executor::setbinning 2
   log::summary "focusvisit: focusing with binning 2."
-  executor::focus 1000 100 false 2
+  executor::focus 2000 250 true 2
   executor::setwindow "1kx1k"
   executor::setbinning 1
   log::summary "focusvisit: focusing with binning 1."
-  executor::focus 1000 100 true 4
-
+  executor::focus 2000 250 true 4
   executor::setfocused
 
   log::summary "focusvisit: finished."
