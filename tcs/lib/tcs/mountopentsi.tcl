808381ebf4ac03c0d5d61c0f3776c2f59a898f17
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2022-11-19 00:56:35 +0100

    Update instrument rotation

--- tcs/lib/tcs/mountopentsi.tcl
+++ tcs/lib/tcs/mountopentsi.tcl
@@ -410,7 +410,7 @@ namespace eval "mount" {
     opentsi::sendcommand "SET POINTING.SETUP.DEROTATOR.SYNCMODE=0"
     opentsi::sendcommand "SET POINTING.SETUP.USE_PORT=3"
     # Hack because the instrument is installed at an angle.
-    opentsi::sendcommand "SET POSITION.INSTRUMENTAL.DEROTATOR\[3\].OFFSET=10"
+    opentsi::sendcommand "SET POSITION.INSTRUMENTAL.DEROTATOR\[3\].OFFSET=23"
     parkhardware
     set end [utcclock::seconds]
     log::info [format "finished initializing after %.1f seconds." [utcclock::diff $end $start]]
