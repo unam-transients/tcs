433554f71e6eeef6e5e4239084eae62594ff0a35
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2022-12-02 11:17:25 -0600

    Factorized code.

--- tcs/lib/tcs/filterwheelfli.tcl
+++ tcs/lib/tcs/filterwheelfli.tcl
@@ -40,35 +40,6 @@ namespace eval "filterwheel" {
     }  
   }
   
-  proc move {newposition} {
-    log::debug "filterwheel: move: moving to position $newposition."
-    variable position
-    variable maxposition
-    variable nfilterwheels
-    set newpositionlist [split $newposition ":"]
-    if {[llength $newpositionlist] != $nfilterwheels} {
-      error "invalid filter position \"$newposition\"."
-    }
-    set index 0
-    while {$index < $nfilterwheels} {
-      if {[lindex $newpositionlist $index] >= [getmaxpositionsingle $index]} {
-        error "invalid filter position \"$newposition\"."
-      }
-      incr index
-    }
-    set index 0
-    while {$index < $nfilterwheels} {
-      log::debug "filterwheel: move: moving filter wheel $index"
-      checkisopen $index
-      movesingle $index [lindex $newpositionlist $index]
-      incr index
-    }
-    log::debug "filterwheel: move: done."
-    variable stoppedtimestamp
-    set stoppedtimestamp ""
-    return
-  }
-  
   proc movesingle {index newposition} {
     log::debug "filterwheel: movesingle: moving filter wheel $index to $newposition."
     variable position
