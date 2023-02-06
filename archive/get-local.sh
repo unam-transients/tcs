0d6958cd3b971a3252cd982ff98984b53bf01cf8
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2023-02-05 21:33:54 -0600

    More clean up.

--- archive/get-local.sh
+++
@@ -1,3 +0,0 @@
-export RSYNC_PASSWORD=Wfs71Smh12                                                
-rsync rsync://archive-en@ratir.astrossp.unam.mx/archive-en/local.tar.gz .       
-rsync rsync://archive-en@ratir.astrossp.unam.mx/archive-en/install-nas.sh .     
