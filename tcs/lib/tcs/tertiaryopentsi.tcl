49fa7892c82f458e26161ed91d4a2546cab2fba6
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2024-09-16 10:54:11 -0700

    Created fake tertiary server as a copy of the covers server.

---
+++ tcs/lib/tcs/tertiaryopentsi.tcl
@@ -0,0 +1,254 @@
+########################################################################
+
+# This file is part of the UNAM telescope control system.
+
+########################################################################
+
+# Copyright © 2017, 2019 Alan M. Watson <alan@astro.unam.mx>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+# AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+# PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
+########################################################################
+
+package require "config"
+package require "opentsi"
+package require "log"
+package require "server"
+
+config::setdefaultvalue "tertiary" "port2name"      "port2"
+config::setdefaultvalue "tertiary" "port3name"      "port3"
+
+package provide "tertiaryopentsi" 0.0
+
+namespace eval "tertiary" {
+
+  ######################################################################
+
+  set server::datalifeseconds        30
+
+  server::setdata "state"            ""
+  server::setdata "requestedtertiary"  ""
+  server::setdata "tertiary"           ""
+  server::setdata "port2cover"       ""
+  server::setdata "port3cover"       ""
+  server::setdata "port2name"        [config::getvalue "tertiary" "port2name"]
+  server::setdata "port3name"        [config::getvalue "tertiary" "port3name"]
+  server::setdata "timestamp"        [utcclock::combinedformat now]
+
+  ######################################################################
+
+  set statuscommand "GET [join {
+    AUXILIARY.COVER.REALPOS
+    AUXILIARY.PORT_COVER[2].REALPOS
+    AUXILIARY.PORT_COVER[3].REALPOS
+    AUXILIARY.COVER.TARGETPOS
+    AUXILIARY.PORT_COVER[2].TARGETPOS
+    AUXILIARY.PORT_COVER[3].TARGETPOS
+  } ";"]"
+
+
+  ######################################################################
+
+  variable tertiary ""
+  variable port2cover ""
+  variable port3cover ""
+  variable tertiarytarget ""
+  variable port2covertarget ""
+  variable port3covertarget ""
+  variable intermediate
+
+  proc updatedata {response} {
+
+    variable tertiary
+    variable port2cover
+    variable port3cover
+    variable tertiarytarget
+    variable port2covertarget
+    variable port3covertarget
+    variable intermediate
+
+    if {[scan $response "%*d DATA INLINE AUXILIARY.COVER.REALPOS=%f" value] == 1} {
+      if {$value == 0} {
+        set tertiary "closed"
+      } elseif {$value == 1.0} {
+        set tertiary "open"
+      } else {
+        set tertiary "intermediate"
+      }
+      return false
+    }
+    if {[scan $response "%*d DATA INLINE AUXILIARY.PORT_COVER\[2\].REALPOS=%f" value] == 1} {
+      if {$value == 0} {
+        set port2cover "closed"
+      } elseif {$value == 1.0} {
+        set port2cover "open"
+      } else {
+        set port2cover "intermediate"
+      }
+      return false
+    }
+    if {[scan $response "%*d DATA INLINE AUXILIARY.PORT_COVER\[3\].REALPOS=%f" value] == 1} {
+      if {$value == 0} {
+        set port3cover "closed"
+      } elseif {$value == 1.0} {
+        set port3cover "open"
+      } else {
+        set port3cover "intermediate"
+      }
+      return false
+    }
+    if {[scan $response "%*d DATA INLINE AUXILIARY.COVER.TARGETPOS=%f" value] == 1} {
+      if {$value == 0} {
+        set tertiarytarget "closed"
+      } else {
+        set tertiarytarget "open"
+      }
+      return false
+    }
+    if {[scan $response "%*d DATA INLINE AUXILIARY.PORT_COVER\[2\].TARGETPOS=%f" value] == 1} {
+      if {$value == 0} {
+        set port2covertarget "closed"
+      } else {
+        set port2covertarget "open"
+      }
+      return false
+    }
+    if {[scan $response "%*d DATA INLINE AUXILIARY.PORT_COVER\[3\].TARGETPOS=%f" value] == 1} {
+      if {$value == 0} {
+        set port3covertarget "closed"
+      } elseif {$value == 1.0} {
+        set port3covertarget "open"
+      }
+      return false
+    }
+
+    if {[regexp {[0-9]+ DATA INLINE } $response] == 1} {
+      log::debug "status: ignoring DATA INLINE response."
+      return false
+    }
+    if {[regexp {[0-9]+ COMMAND COMPLETE} $response] != 1} {
+      log::warning "unexpected controller response \"$response\"."
+      return true
+    }
+
+    set timestamp [utcclock::combinedformat "now"]
+
+    set lasttertiary         [server::getdata "tertiary"]
+    set lastport2cover     [server::getdata "port2cover"]
+    set lastport3cover     [server::getdata "port3cover"]
+    
+    if {![string equal $lasttertiary ""] && ![string equal $tertiary $lasttertiary]} {
+      log::info "tertiary changed from \"$lasttertiary\" to \"$tertiary\"."
+    }
+    if {![string equal $lastport2cover ""] && ![string equal $port2cover $lastport2cover]} {
+      log::info "port 2 cover changed from \"$lastport2cover\" to \"$port2cover\"."
+    }
+    if {![string equal $lastport3cover ""] && ![string equal $port3cover $lastport3cover]} {
+      log::info "port 3 cover changed from \"$lastport3cover\" to \"$port3cover\"."
+    }
+
+    if {
+      [string equal $tertiary     "intermediate"] ||
+      [string equal $port2cover "intermediate"] ||
+      [string equal $port3cover "intermediate"]
+    } {
+      set intermediate true
+    } else {
+      set intermediate false
+    }
+    
+    server::setdata "timestamp"        $timestamp
+    server::setdata "tertiary"           $tertiary
+    server::setdata "port2cover"       $port2cover
+    server::setdata "port3cover"       $port3cover
+
+    server::setstatus "ok"
+
+    return true
+  }
+
+  proc waitwhilemoving {} {
+    log::info "waiting while moving."
+    variable intermediate
+    set startingdelay 10
+    set settlingdelay 1
+    set start [utcclock::seconds]
+    while {[utcclock::diff now $start] < $startingdelay} {
+      coroutine::after 1000
+    }
+    while {$intermediate} {
+      coroutine::yield
+    }
+    set settle [utcclock::seconds]
+    while {[utcclock::diff now $settle] < $settlingdelay} {
+      coroutine::yield
+    }
+    log::info "finished waiting while moving."
+  }
+
+  ######################################################################
+  
+  proc checkhardwarefor {action} {
+    switch $action {
+      "reset" {
+      }
+      default {
+        opentsi::checkreadystate "operational"
+      }
+    }
+  }
+  
+  proc initializehardware {} {
+    closehardware
+  }
+  
+  proc stophardware {} {
+    server::setdata "requestedtertiary" ""
+    opentsi::sendcommand "SET TELESCOPE.STOP=1"
+  }
+  
+  proc openhardware {} {
+    server::setdata "requestedtertiary" "open"
+    opentsi::sendcommand [format "SET AUXILIARY.PORT_COVER\[2\].TARGETPOS=1"]
+    opentsi::sendcommand [format "SET AUXILIARY.PORT_COVER\[3\].TARGETPOS=1"]
+    opentsi::sendcommand [format "SET AUXILIARY.COVER.TARGETPOS=1"]
+    waitwhilemoving
+    if {![string equal [server::getdata "tertiary"] "open"]} {
+      error "the tertiary did not open."
+    }
+  }
+  
+  proc closehardware {} {
+    server::setdata "requestedtertiary" "closed"
+    opentsi::sendcommand [format "SET AUXILIARY.PORT_COVER\[2\].TARGETPOS=0"]
+    opentsi::sendcommand [format "SET AUXILIARY.PORT_COVER\[3\].TARGETPOS=0"]
+    opentsi::sendcommand [format "SET AUXILIARY.COVER.TARGETPOS=0"]
+    waitwhilemoving
+    if {![string equal [server::getdata "tertiary"] "closed"]} {
+      error "the tertiary did not close."
+    }
+  }
+  
+  ######################################################################
+  
+  proc start {} {
+    opentsi::start $tertiary::statuscommand tertiary::updatedata
+    server::newactivitycommand "starting" "started" tertiary::startactivitycommand
+  }
+
+  ######################################################################
+
+}
+
+source [file join [directories::prefix] "lib" "tcs" "tertiary.tcl"]
---
+++ tcs/lib/tcs/tertiaryserver.tcl
@@ -0,0 +1,73 @@
+########################################################################
+
+# This file is part of the UNAM telescope control system.
+
+########################################################################
+
+# Copyright © 2010, 2011, 2017, 2019 Alan M. Watson <alan@astro.unam.mx>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+# AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+# PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
+########################################################################
+
+package require "config"
+package require "log"
+package require "server"
+
+package require "tertiary[config::getvalue "tertiary" "type"]"
+
+package provide "tertiaryserver" 0.0
+
+namespace eval "tertiaryserver" {
+
+  ######################################################################
+
+  proc slaveinitialize {} {
+    tertiary::initialize
+  }
+
+  proc slavestop {} {
+    tertiary::stop
+  }
+
+  proc slavereset {} {
+    tertiary::reset
+  }
+
+  proc slaveopen {} {
+    tertiary::open
+  }
+
+  proc slaveclose {} {
+    tertiary::close
+  }
+
+  proc configureslave {slave} {
+    interp alias $slave initialize {} tertiaryserver::slaveinitialize
+    interp alias $slave stop       {} tertiaryserver::slavestop
+    interp alias $slave reset      {} tertiaryserver::slavereset
+    interp alias $slave open       {} tertiaryserver::slaveopen
+    interp alias $slave close      {} tertiaryserver::slaveclose
+  }
+
+  ######################################################################
+
+  proc start {} {
+    tertiary::start
+    server::listen tertiary tertiaryserver::configureslave
+  }
+
+  ######################################################################
+
+}
