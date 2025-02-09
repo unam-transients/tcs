e34a82363cda356e1dbe2729b93e8d175d2f6fff
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2025-02-09 17:39:52 -0600

    Create alerts page.

--- tcs/sys/install-project-colibri.sh
+++ tcs/sys/install-project-colibri.sh
@@ -104,7 +104,7 @@ EOF
 *      *  *  *  *  cd /usr/local/var/www/tcs/; sh plots.sh >plots.txt 2>&1
 */5    *  *  *  *  tcs logsensors
 *      *  *  *  *  mkdir -p /usr/local/var/www/tcs/alerts/; rsync --delete --dirs /usr/local/var/tcs/alerts/ /usr/local/var/www/tcs/alerts/
-*      *  *  *  *  tcs makealertspage
+*      *  *  *  *  tcs request selector makealertspage
 EOF
     ;;
   esac
