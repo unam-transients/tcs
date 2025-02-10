94a3c89f0a9b2bacbbef680f623dc5ce53f96e4c
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2025-02-10 08:45:06 -0600

    Expose a copy of the block pool to the web server.

--- tcs/sys/install-project-coatli.sh
+++ tcs/sys/install-project-coatli.sh
@@ -96,6 +96,7 @@ EOF
 *   *  * *  *  sh /usr/local/var/www/tcs/plots.sh
 */5 *  * *  *  tcs logsensors
 *      *  *  *  *  mkdir -p /usr/local/var/www/tcs/alerts/; rsync --delete --dirs /usr/local/var/tcs/alerts/ /usr/local/var/www/tcs/alerts/
+*      *  *  *  *  mkdir -p /usr/local/var/www/tcs/blocks/; rsync --delete --dirs /usr/local/var/tcs/blocks/ /usr/local/var/www/tcs/blocks/
 *      *  *  *  *  tcs request selector makealertspage
 EOF
     ;;
