132cea397730b4e437967cedd49867ae448eedf3
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2026-09-25 20:43:48 -0600

    Start the watchdog server early Fixes #158

--- tcs/lib/tcs/watchdog.tcl
+++ tcs/lib/tcs/watchdog.tcl
@@ -1,9 +1,6 @@
 ########################################################################
-
 # This file is part of the UNAM telescope control system.
-
 ########################################################################
-
 # Copyright © 2010, 2011, 2013, 2014, 2017, 2019 Alan M. Watson <alan@astro.unam.mx>
 #
 # Permission to use, copy, modify, and distribute this software for any
@@ -19,9 +16,7 @@
 # PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
 # TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
-
 ########################################################################
-
 package require "client"
 package require "coroutine"
 package require "server"
@@ -30,132 +25,117 @@ package require "utcclock"
 package provide "watchdog" 0.0
 
 namespace eval "watchdog" {
-
   variable servers [config::getvalue "watchdog" "monitoredservers"]
   variable problemtoleranceseconds [config::getvalue "watchdog" "problemtoleranceseconds"]
 
   ######################################################################
-
-  variable problemservers   {}
+  variable problemservers {}
   variable problemtimestamp ""
   variable problemnotified false
   variable enabled false
-  
-  ######################################################################
 
-  server::setdata "enabled"          false
-  server::setdata "servers"          $servers
-  server::setdata "problemservers"   {}
+  ######################################################################
+  server::setdata "enabled" false
+  server::setdata "servers" $servers
+  server::setdata "problemservers" {}
   server::setdata "problemtimestamp" ""
-  server::setdata "timestamp"        [utcclock::combinedformat "now"]
+  server::setdata "timestamp" [utcclock::combinedformat "now"]
 
   ######################################################################
-  
   proc notify {} {
-
     variable problemservers
     variable problemtimestamp
 
-    log::warning [format \
-      "persistent problems since %s with: %s" \
-      [utcclock::format $problemtimestamp 0] [join $problemservers " "] \
-    ]
-    
+    log::warning \
+      [format "persistent problems since %s with: %s" [utcclock::format $problemtimestamp 0] \
+      [join $problemservers " "] ]
+
     variable enabled
     if {!$enabled} {
       return
     }
 
-    set message [format \
-      "watchdog: persistent problems since %s with: %s" \
-      [utcclock::format $problemtimestamp 0] [join $problemservers " "] \
-    ]
-    
+    set message \
+      [format "watchdog: persistent problems since %s with: %s" [utcclock::format $problemtimestamp 0] \
+      [join $problemservers " "] ]
+
     log::info "sending emergency pushover message \"$message\"."
     if {[catch {
-      exec "[directories::prefix]/bin/tcs" "sendpushover" \
-        "-P" "emergency" \
-        "-s" "Watchdog" \
-        "emergency" "$message"
+      exec "[directories::prefix]/bin/tcs" "sendpushover" "-P" "emergency" "-s" "Watchdog" "emergency" "$message"
     }]} {
       log::warning "unable to send emergency pushover message \"$message\"."
     }
-
   }
-  
+
   ######################################################################
-  
   proc monitorservers {} {
-  
-    variable servers
-    variable problemtoleranceseconds
-    variable problemservers
-    variable problemtimestamp
-    variable problemnotified
+    if {[catch {
+      variable servers
+      variable problemtoleranceseconds
+      variable problemservers
+      variable problemtimestamp
+      variable problemnotified
+
+      set lastproblemservers $problemservers
+
+      set problemservers {}
+      foreach server $servers {
+
+        if {[catch {client::update $server}]} {
+          log::info "$server: no response to update."
+          lappend problemservers $server
+          continue
+        }
+
+        set status [client::getstatus $server]
+        if {![string equal $status "ok"]} {
+          log::info "$server: status is $status."
+          lappend problemservers $server
+          continue
+        }
+
+        set activity [client::getdata $server "activity"]
+        if {[string equal $activity "error"]} {
+          log::info "$server: activity is $activity."
+          lappend problemservers $server
+          continue
+        }
+
+      }
+
+      if {[llength $problemservers] == 0} {
+        if {[llength $lastproblemservers] > 0} {
+          log::info "no servers have problems."
+        }
+        set problemtimestamp ""
+      } else {
+        if {[llength $lastproblemservers] == 0} {
+          set problemtimestamp [utcclock::combinedformat "now"]
+        }
+        log::info [format "problems since %s with: %s" [utcclock::format $problemtimestamp 0] [join $problemservers " "] ]
+      }
     
-    set lastproblemservers $problemservers
+      variable enabled
+      server::setdata "enabled"          $enabled
+      server::setdata "servers"          $servers
+      server::setdata "problemservers"   $problemservers
+      server::setdata "problemtimestamp" $problemtimestamp
+      server::setdata "timestamp"        [utcclock::combinedformat "now"]
 
-    set problemservers {}
-    foreach server $servers {
-
-      if {[catch {client::update $server}]} {
-        log::info "$server: no response to update."
-        lappend problemservers $server
-        continue
+      if {[llength $problemservers] == 0} {
+        set problemnotified false
+      } elseif {[utcclock::diff now $problemtimestamp] > $problemtoleranceseconds} {
+        if {!$problemnotified} {
+          notify
+          set problemnotified true
+        }
       }
-      
-      set status [client::getstatus $server]
-      if {![string equal $status "ok"]} {
-        log::info "$server: status is $status."
-        lappend problemservers $server
-        continue
-      }
-
-      set activity [client::getdata $server "activity"]
-      if {[string equal $activity "error"]} {
-        log::info "$server: activity is $activity."
-        lappend problemservers $server
-        continue
-      }
-        
+    } message]} {
+      log::warning "while monitoring servers: $message"
     }
-    
-    if {[llength $problemservers] == 0} {
-      if {[llength $lastproblemservers] > 0} {
-        log::info "no servers have problems."
-      }
-      set problemtimestamp ""
-    } else {
-      if {[llength $lastproblemservers] == 0} {
-        set problemtimestamp [utcclock::combinedformat "now"]
-      }
-      log::info [format \
-        "problems since %s with: %s" \
-        [utcclock::format $problemtimestamp 0] [join $problemservers " "] \
-      ]
-    }
-    
-    variable enabled
-    server::setdata "enabled"          $enabled
-    server::setdata "servers"          $servers
-    server::setdata "problemservers"   $problemservers
-    server::setdata "problemtimestamp" $problemtimestamp
-    server::setdata "timestamp"        [utcclock::combinedformat "now"]
-
-    if {[llength $problemservers] == 0} {
-      set problemnotified false
-    } elseif {[utcclock::diff now $problemtimestamp] > $problemtoleranceseconds} {
-      if {!$problemnotified} {
-        notify
-        set problemnotified true
-      }
-    }
-
-
   }
 
   ######################################################################
-
   proc enable {} {
     log::summary "enabling."
     variable enabled
@@ -164,7 +144,7 @@ namespace eval "watchdog" {
     log::summary "finished enabling."
     return
   }
-  
+
   proc disable {} {
     log::summary "disabling."
     variable enabled
@@ -173,28 +153,27 @@ namespace eval "watchdog" {
     log::summary "finished disabling."
     return
   }
-  
+
   proc reset {} {
     log::summary "resetting."
     server::checkstatus
     server::checkactivityforreset
     variable problemservers
     variable problemtimestamp
-    set problemservers   {}
+    set problemservers {}
     set problemtimestamp ""
     log::summary "finished resetting."
     return
   }
 
   ######################################################################
-
   set server::datalifeseconds 120
 
   proc start {} {
+    coroutine::after 10000
     server::setrequestedactivity "idle"
     server::setactivity "idle"
     server::setstatus "ok"
     coroutine::every 30000 watchdog::monitorservers
   }
-
 }
